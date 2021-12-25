b0c0428612a4ce6b7aa1249c82de103adc81b84e
Cvijetin Mladjenovic <mladjenovic.cvijetin@gmail.com>
2021-12-25 18:04:18 +0100

    Update AccountBalance.js

--- client/src/components/AccountBalance.js
+++ client/src/components/AccountBalance.js
@@ -7,6 +7,7 @@ import { Form, Button, Table, Modal } from "react-bootstrap";
 const HomePage = () => {
   const token = localStorage.getItem("REACT_TOKEN_AUTH_KEY");
   const [accountBalance, setAccountBalance] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const requestOptions = {
@@ -21,6 +22,7 @@ const HomePage = () => {
       .then((data) => {
         console.log("------------aaaa------------\n", data);
         setAccountBalance(data);
+        setLoading(false);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -29,23 +31,27 @@ const HomePage = () => {
     <div className="container">
       <h1>Your account balance</h1>
 
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>Account number</th>
-            <td>{accountBalance.accounts[0].id}</td>
-          </tr>
-          <tr>
-            <th>Balance</th>
-            <td>{accountBalance.accounts[0].balance}</td>
-          </tr>
-          <tr>
-            <th>Currency</th>
-            <td>{accountBalance.accounts[0].currency}</td>
-          </tr>
-        </thead>
-        <tbody></tbody>
-      </Table>
+      {loading ? (
+        <></>
+      ) : (
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Account number</th>
+              <td>{accountBalance.accounts[0].id}</td>
+            </tr>
+            <tr>
+              <th>Balance</th>
+              <td>{accountBalance.accounts[0].balance}</td>
+            </tr>
+            <tr>
+              <th>Currency</th>
+              <td>{accountBalance.accounts[0].currency}</td>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </Table>
+      )}
     </div>
   );
 };
